54db0b0aca5445dc44def255245b03ef35709f98
eduardaleksanyan <eduard.aleksanyan@gmail.com>
2024-07-30 21:37:42 +0400

    Test

--- backend/src/modules/taskRegistrations/schemas/tasks.schema.ts
+++ backend/src/modules/taskRegistrations/schemas/tasks.schema.ts
@@ -2,7 +2,6 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import { TaskType } from '../../../constants/constants';
 
-
 @Schema({ collection: 'tasks' })
 export class Tasks extends Document {
   @Prop({ required: true })
